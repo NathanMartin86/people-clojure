573e64db8fbc6f23fb36013a3f7da62698fdd3de
macbookair <nj.martin123@gmail.com>
2015-11-25 10:57:51 -0500

    display people in .html page

--- src/people_clojure/core.clj
+++ src/people_clojure/core.clj
@@ -1,13 +1,15 @@
 (ns people-clojure.core
   (:require [clojure.string :as str]
-            [clojure.walk :as walk])
+            [clojure.walk :as walk]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main [& args]
+(defn read-people []
   (let [people (slurp "people.csv")
         people (str/split-lines people)
         people (map (fn [line]
-                         (str/split line #","))
+                      (str/split line #","))
                     people)
         header (first people)
         people (rest people)
@@ -19,9 +21,50 @@
                     people)
         people (walk/keywordize-keys people)
         people (filter (fn [line]
-                            (= "Brazil" (:country line)))
+                         (= "Brazil" (:country line)))
                        people)]
-    (spit "filtered_people.edn"
-          (pr-str people))))
+    #_(spit "filtered_people.edn"
+          (pr-str people))
+    people))
+
+(defn people-html []
+  (let [people (read-people)]
+    (map (fn [line]
+           [:p
+            (str(:first_name line)
+                " "
+                (:last_name line)
+                )])
+         people)))
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                      [:a {:href "http://www.theironyard.com"}
+                       "the Iron Yard"]
+                      [:br]
+                      (people-html)]])})
+
+(defn -main [& args]
+  (j/run-jetty #'handler {:port 3000 :join? false}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
